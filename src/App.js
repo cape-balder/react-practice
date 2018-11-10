a1490743646e8ef805b003431acaf280b4629926
cape-balder <cape.balder@gmail.com>
2018-11-10 19:34:05 +0700

    Passing method references bind(this)

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Was clicked!')
     // Don't do this: this.state.persons[0].name = 'Maximilian'
     this.setState({
       persons:[
-        {name: 'Maximilian', age:29},
+        {name: newName, age:29},
         {name: 'Manu', age:28},
         {name: 'Staphanie', age:26}
       ]
@@ -29,10 +29,17 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello world</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={this.switchNameHandler.bind(this,'Maximilian')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this,'Max!')}>My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
   }
