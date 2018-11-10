f1d61c08518660215e7f9e8899b6237f774642ea
cape-balder <cape.balder@gmail.com>
2018-11-10 17:29:25 +0700

    Working with Props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
-
+ 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello world</h1>
-        <Person/>
+        <Person name="Max" age="29"/>
+        <Person name="Manu" age="28"/>
+        <Person name="Staphanie" age="27"/>
       </div>
     );
   }
