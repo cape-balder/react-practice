6ef490237a1121597deea8acc411fc02c2408047
cape-balder <cape.balder@gmail.com>
2018-11-10 20:01:11 +0700

    Two way binding

--- src/App.js
+++ src/App.js
@@ -25,6 +25,16 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons:[
+        {name: 'Max', age:29},
+        {name: event.target.value, age:28},
+        {name: 'Staphanie', age:27}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -36,7 +46,8 @@ class App extends Component {
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this,'Max!')}>My Hobbies: Racing</Person>
+          click={this.switchNameHandler.bind(this,'Max!')}
+          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age}/>
