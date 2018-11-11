d9a18b5dc3ae9a416a244635580de27e798d1dba
cape-balder <cape.balder@gmail.com>
2018-11-11 14:05:16 +0700

    Inline style

--- src/App.js
+++ src/App.js
@@ -36,10 +36,20 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hello world</h1>
-        <button onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
+        <button
+          style={style} 
+          onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/>
